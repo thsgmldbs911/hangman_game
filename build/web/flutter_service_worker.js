141a9d051e6d01196d8c4124e7ca699fce565ed6
손희윤 <thsgmldbs911@gmail.com>
2025-08-07 22:55:52 +0900

    Update web build files for GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d702e76edb30071b67e0e863b54a89fc
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "c47019c6975570d1d933fd4f80604219",
+"flutter_bootstrap.js": "1806de7959d7a6b0c6345e22db3f386d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
